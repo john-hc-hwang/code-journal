ab61c3f75beea355571c7d103ecff5e880564035
John Hwang <johnhchwang@gmail.com>
2021-05-24 13:36:00 -0700

    current work

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
 /* global data */
 /* exported data */
+
+var $photoUrl = document.querySelector('#photoUrl');
+var $img = document.querySelector('img');
+$photoUrl.addEventListener('input', function (event) {
+  $img.setAttribute('src', event.target.value);
+  if (event.target.value === '') {
+    $img.setAttribute('src', 'images/placeholder-image-square.jpg');
+  }
+});
